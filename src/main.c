92b5606871cffe181adfd5c5b91ccb14886fbe5c
blue <midnight@nightmarish.blue>
2024-07-08 18:30:51 +0100

    Reject nondigits in the given port, with a snazzy error message

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include "sock.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 int main(int argc, char* argv[])
 {
@@ -20,6 +22,20 @@ int main(int argc, char* argv[])
         fprintf(stderr, "Error: no port given\n%s\n", usagemsg);
         return 2;
     }
+
+    char* portstr = argv[i];
+    for (size_t i = 0; portstr[i]; i++)
+        if (!isdigit((unsigned char) portstr[i]))
+        {
+            fputs("Error: given port '", stderr); // 19 chars
+            i += 19; // we're breaking this loop anyway
+            fprintf(stderr, "%s' invalid\n", portstr);
+            for (size_t j = 0; j < i; j++) putc(' ', stderr);
+            fputs("^\n", stderr);
+            return 1;
+        }
     
+    int portn = atoi(portstr);
+    servesock(portn);
     return 0;
 }
