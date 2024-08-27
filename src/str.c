a68e8fc4fb42bc58dd7ef7c910141c9a3601a956
blue <midnight@nightmarish.blue>
2024-08-27 17:56:28 +0100

    Swap msprintf implementation out of nonstandard vasprintf

--- src/str.c
+++ src/str.c
@@ -1,15 +1,27 @@
 #include "str.h"
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <stdarg.h>
 
 char* msprintf(const char* fmt, ...)
 {
-    char* str;
     va_list args;
     va_start(args, fmt);
-    int r = vasprintf(&str, fmt, args);
+    int size = vsnprintf(NULL, 0, fmt, args);
     va_end(args);
-    if (r == -1) return NULL;
+    if (size == -1) return NULL;
+
+    char* str = malloc((size + 1) * sizeof(char));
+    if (!str) return NULL;
+
+    va_start(args, fmt);
+    size = vsnprintf(str, size + 1, fmt, args);
+    va_end(args);
+    if (size == -1)
+    {
+        free(str);
+        return NULL;
+    }
     return str;
 }
